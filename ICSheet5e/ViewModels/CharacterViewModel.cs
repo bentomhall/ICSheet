3d149fa62870679383f53418a370e5b551a90863
Ben Hall <bentomhall@gmail.com>
2016-02-19 21:53:52 -0500

    adding view model/command stuff

---
+++ ICSheet5e/ViewModels/CharacterViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ICSheet5e.ViewModels
+{
+    class CharacterViewModel : INotifyPropertyChanged
+    {
+        private Model.Character character;
+        public Model.Character Character 
+        {
+            get { return character; }
+            set
+            {
+                character = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        public void NewCharacterHandler(object sender, EventArgs e)
+        {
+            character = new Model.Character();
+            NotifyPropertyChanged("Character");
+        }
+
+        public void SaveCharacterHandler(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
