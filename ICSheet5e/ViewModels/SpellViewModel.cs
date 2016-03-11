ea8ebc0f82d134d10558ca650f1105141a5de8c2
Ben Hall <bentomhall@gmail.com>
2016-03-11 17:04:48 -0500

    stupid missed files

---
+++ ICSheet5e/ViewModels/SpellViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSheet5e.ViewModels
+{
+    public class SpellViewModel : BaseViewModel
+    {
+        private bool isSpellKnown = false;
+        private Model.Spell _spell;
+
+        public bool SpellKnown
+        {
+            get { return isSpellKnown; }
+            set
+            {
+                isSpellKnown = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public SpellViewModel(Model.Spell spell)
+        {
+            _spell = spell;
+        }
+
+        public Model.Spell Spell { get { return _spell; } }
+
+        public string Name { get { return _spell.Name; } }
+        public bool IsPrepared { get { return _spell.IsPrepared; } }
+        public int Level { get { return _spell.Level; } }
+
+        
+    }
+}
