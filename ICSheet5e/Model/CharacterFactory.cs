2cb984f17fde45786ab0cb8b053069722a5be679
Ben Hall <bentomhall@gmail.com>
2016-03-20 17:58:41 -0400

    stupid untracking of random files....

---
+++ ICSheet5e/Model/CharacterFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ICSheet5e.Model
+{
+    class CharacterFactory
+    {
+        private string characterName;
+        private Race characterRace;
+        private List<Tuple<CharacterClassType, int>> characterLevels;
+        private ItemDataBase itemDB;
+        private SpellManager spellDB;
+        private XMLFeatureFactory featureFactory = new XMLFeatureFactory();
+
+        internal CharacterFactory(string name, Race race, IEnumerable<Tuple<CharacterClassType, int>> levels, ItemDataBase items, SpellManager spells)
+        {
+            characterLevels = new List<Tuple<CharacterClassType,int>>(levels);
+            characterName = name;
+            characterRace = race;
+            itemDB = items;
+            spellDB = spells;
+        }
+
+        internal Character Build()
+        {
+            var c = new Character(characterName, characterLevels, characterRace);
+            c.ItemDB = itemDB;
+            c.SpellDB = spellDB;
+            var raceFeatures = featureFactory.RacialFeatures(characterRace);
+            foreach (var f in raceFeatures)
+            {
+                c.AddFeature(f);
+            }
+
+            foreach (var clsLvl in characterLevels)
+            {
+                var classFeatures = featureFactory.ClassFeatures(clsLvl.Item1);
+                foreach (var f in classFeatures) { c.AddFeature(f); }
+            }
+            return c;
+        }
+
+    }
+}
