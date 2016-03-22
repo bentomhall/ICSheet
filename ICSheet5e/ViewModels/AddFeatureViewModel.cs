ea2c81c2fa1699077a2f94239eaffa8a935eb7f0
Ben Hall <bentomhall@gmail.com>
2016-03-22 11:46:06 -0400

    visual changes (mainly with borders)

---
+++ ICSheet5e/ViewModels/AddFeatureViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICSheet5e.ViewModels
+{
+    public class AddFeatureViewModel : BaseViewModel
+    {
+        public AddFeatureViewModel() {}
+
+        public string FeatureName { get; set; }
+        public string Uses { get; set; }
+        public string FeatureText { get; set; }
+        public int StartingLevel { get; set; }
+
+        public Model.MartialFeature ToFeature()
+        {
+            var feature = new Model.MartialFeature(FeatureName, FeatureText, Uses ?? "");
+            feature.MinimumLevel = StartingLevel;
+            return feature;
+        }
+    }
+}
